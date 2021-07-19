da007a27e6377520abf101e534f2b1dba4c519a6
KiSchnelle <kischnelle@web.de>
2021-07-19 16:09:23 +0200

    work

---
+++ test.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Make sure to have all nodes time synchronized by for example using ntp !!
+ANSIBLE_USER=
+ANSIBLE_USER_PASSWORD=
+
+# for Ubuntu uncomment this
+apt update
+apt upgrade --yes
+apt install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt update
+apt install ansible-base --yes
+apt install git --yes
+adduser --home /var/lib/ansible $ANSIBLE_USER
+echo $ANSIBLE_USER:$ANSIBLE_USER_PASSWORD | chpasswd
+usermode -aG sudo $ANSIBLE_USER
+
+# for CentOS uncomment this
+# yum update --yes
+# yum install epel-release --yes
+# yum install ansible --yes
+# yum install git --yes
+# adduser --home /var/lib/ansible $ANSIBLE_USER
+# echo $ANSIBLE_USER:$ANSIBLE_USER_PASSWORD | chpasswd
+# usermod -aG wheel $ANSIBLE_USER
+
+
+# create folder for stuff we need
+mkdir -p /var/log/ansible/roles
+
+
+ansible-galaxy collection install ansible.posix
+cd /var/log/ansible/roles
+git clone https://github.com/KiSchnelle/role_nvidia.git
+
+cd /var/log/ansible
+mv /var/log/ansible/roles/role_nvidia/run.yml .
+ansible-playbook run.yml --user=$ANSIBLE_USER --extra-vars "ansible_sudo_pass=$ANSIBLE_USER_PASSWORD" >> log.txt
+
+cd /var/log/ansible
+
